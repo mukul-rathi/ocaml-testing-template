555791333b9c2fe4d60833717fdda9cbf5c89845
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-06 10:54:01 +0000

    Add Reason example to template

--- tests/test_hello_world.ml
+++ tests/test_hello_world.ml
@@ -1,4 +1,10 @@
-let test_something () = Alcotest.(check int) "same int" 1 (Hello_world.id 1)
+open Lib
+
+(* OCaml and Reason interoperate seamlessly! *)
+let test_something () =
+  Alcotest.(check string)
+    "same string" "Hey Professor!"
+    (Something.greeting Something.Teacher)
 
 let test_other_thing =
   QCheck.Test.make ~count:1000 ~name:"zero"
@@ -6,7 +12,9 @@ let test_other_thing =
     (fun i -> Hello_world.zero i = 0)
 
 let () =
-  let foo = List.map QCheck_alcotest.to_alcotest [test_other_thing] in
+  let foo = List.map QCheck_alcotest.to_alcotest [ test_other_thing ] in
   Alcotest.run "Utils"
-    [ ("string-case", [Alcotest.test_case "Something" `Quick test_something])
-    ; ("foo", foo) ]
+    [
+      ("string-case", [ Alcotest.test_case "Something" `Quick test_something ]);
+      ("foo", foo);
+    ]
