33173f75284535b3305d7c5817fce33771dae7cc
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-16 22:08:39 +0000

    Refactor tests structure

--- src/inline-expect-tests/foo.ml
+++ src/inline-expect-tests/foo.ml
@@ -1,3 +1,4 @@
 let%expect_test "Test name" =
-  print_string "Hello World"; (* or equiv code that includes print statements *)
+  print_string "Hello World";
+  (* or equiv code that includes print statements *)
   [%expect {|Hello World|}]
--- tests/expect/test_something.ml
+++ tests/expect/test_something.ml
@@ -1,5 +1,9 @@
 open Lib.Something
 
-let%expect_test "Test greeting" =
-  print_string (greeting Teacher); 
+let%expect_test "Test greeting teacher" =
+  print_string (greeting Teacher);
   [%expect {|Hey Professor!|}]
+
+let%expect_test "Test greeting student" =
+  print_string (greeting (Student "Bob"));
+  [%expect {|Hey, Bob.|}]
