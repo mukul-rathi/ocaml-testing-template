a52b9e3193c07d8857e1cd5c45b3cd2a2fb8a400
Mukul Rathi <mukul.rathi@gmail.com>
2020-01-16 22:10:50 +0000

    Modify inline expect tests

--- src/inline-expect-tests/foo.ml
+++ src/inline-expect-tests/foo.ml
@@ -1,4 +1,7 @@
+let print_hello_world () = print_string "Hello World"
+
+(* Test in same file as source code *)
 let%expect_test "Test name" =
-  print_string "Hello World";
+  print_hello_world ();
   (* or equiv code that includes print statements *)
   [%expect {|Hello World|}]
